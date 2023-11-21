f09616ed0b09bb7bd6844e1a70d6687992be1673
nehaAnnareddy <89593420+nehaAnnareddy@users.noreply.github.com>
2023-11-20 20:35:46 -0500

    Update Hospital.java

--- src/main/java/edu/uc/cs3003/medava/Hospital.java
+++ src/main/java/edu/uc/cs3003/medava/Hospital.java
@@ -1,1 +1,29 @@
 package edu.uc.cs3003.medava;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Hospital {
+    
+    public Hospital(String hospitalName) {
+        name = hospitalName;
+    }
+
+    void receive(Transporter t) {
+        while (!t.isEmpty()) {
+            Shippable unloaded = t.unload();
+            System.out.println(String.format("Checking whether Hospital can receive %s.", unloaded.getMedicineName()));
+            if (unloaded.getSchedule() != MedicineSchedule.Uncontrolled) {
+                System.out.println(String.format("Hospital cannot receive controlled substances and %s is a %s.",
+                        unloaded.getMedicineName(), unloaded.getSchedule().asString()));
+            } else {
+                System.out.println(String.format("Accepted a shipment of %s.", unloaded.getMedicineName()));
+            }
+        }
+    }
+
+    public String name() {
+        return name;
+    }
+
+    private String name;
+}
